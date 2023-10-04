9fad5d5d75404a1183021f411e8c18589349c6fb
Felipe dos Santos Oliveira <felipedossantosads@gmail.com>
2023-10-04 20:46:57 -0300

    feat: adicionando base para o calculo de ir

--- calculate.php
+++ calculate.php
@@ -7,15 +7,15 @@ if(isset($_GET['option'])){
     switch ($opcao) {
         case 'ferias':
             // Calcular o valor das férias
-            $_SESSION['ferias'] = $_SESSION['salario'] * 0.33; // Exemplo de cálculo de férias
+            $_SESSION['ferias'] = $_SESSION['salario'] * 0.33; 
             break;
         case 'ir':
             // Calcular o valor do IR
-            $_SESSION['ir'] = $_SESSION['salario'] * 0.15; // Exemplo de cálculo de IR
+            $_SESSION['ir'] = ir($_SESSION['salario']);
             break;
         case 'aumento':
             // Calcular o aumento de salário
-            $_SESSION['aumento'] = $_SESSION['salario'] * 0.1; // Exemplo de cálculo de aumento
+            $_SESSION['aumento'] = $_SESSION['salario'] * 0.1;
             break;
         default:
             break;
@@ -24,6 +24,20 @@ if(isset($_GET['option'])){
     $_SESSION['opcao'] = $opcao;
     if(!isset($_SESSION[$opcao])){
         $_SESSION[$opcao] = 1;
+    }
+}
+
+function ir($salario){
+    switch ($salario){
+        case($salario <= 2112):
+            return 0;
+        case($salario <= 2826.65):
+            return ($salario - 1903.98) * 0.075;
+        case($salario <= 3751.05):
+            $temp = (2826.65 * 0.075) + (($salario - 2826.65)*0.15);
+            return $temp;
+        default:
+            return 0;
     }
 }
 
