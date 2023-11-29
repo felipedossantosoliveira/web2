52dd9b53d5de81e2a461ea82d55e2e9e8f5c53af
Felipe dos Santos Oliveira <felipedossantosads@gmail.com>
2023-11-29 20:51:39 +0000

    feat: adicionando criação de tabelas

--- database/create.sql
+++ database/create.sql
@@ -1,28 +1,29 @@
 CREATE DATABASE IF NOT EXISTS company;
 
 USE company;
-DROP TABLE IF EXISTS companies;
-DROP TABLE IF EXISTS departaments;
-DROP TABLE IF EXISTS employees;
 
-CREATE TABLE companies(
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS departaments;
+DROP TABLE IF EXISTS companies;
+
+CREATE TABLE companies (
     id BIGINT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     cnpj VARCHAR(14) NOT NULL
 );
 
-CREATE TABLE departaments(
+CREATE TABLE departaments (
     id BIGINT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     company_id BIGINT  NOT NULL,
-    FOREIGN KEY (company_id) REFERENCES companies(id)
+    FOREIGN KEY (company_id) REFERENCES companies(id) ON DELETE CASCADE
 );
 
-CREATE TABLE employees(
+CREATE TABLE employees (
     id BIGINT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     cpf VARCHAR(11) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
     departament_id BIGINT  NOT NULL,
-    FOREIGN KEY (departament_id) REFERENCES departaments(id)
+    FOREIGN KEY (departament_id) REFERENCES departaments(id) ON DELETE CASCADE
 );
